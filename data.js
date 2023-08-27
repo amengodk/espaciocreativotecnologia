df2c42653f9814b8f04192a82bc091521bd2d1da
amengodk <137509270+amengodk@users.noreply.github.com>
2023-08-27 07:46:31 +0100

    Upload

---
+++ data.js
@@ -0,0 +1,176 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_20230623_123352_567",
+      "name": "IMG_20230623_123352_567",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.8092272371178577,
+        "pitch": 0.1993818163077119,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0973260139346337,
+          "pitch": 0.20585283553968559,
+          "rotation": 7.0685834705770345,
+          "target": "1-img_20230623_123348_040"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.864867739543536,
+          "pitch": -0.193340468684017,
+          "title": "Zona de proyección",
+          "text": "En esta zona se proyectarán contenidos teóricos, exposiciones del alumnado,etc"
+        },
+        {
+          "yaw": -2.804846798214239,
+          "pitch": -0.05061126513467862,
+          "title": "Zona de trabajo con TIC",
+          "text": "Portátiles para trabajo en grupo con TIC"
+        }
+      ]
+    },
+    {
+      "id": "1-img_20230623_123348_040",
+      "name": "IMG_20230623_123348_040",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.4847551607677367,
+        "pitch": 0.07731543584035805,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.46770627228378103,
+          "pitch": 0.6345532465101815,
+          "rotation": 18.84955592153877,
+          "target": "0-img_20230623_123352_567"
+        },
+        {
+          "yaw": 2.871207651450904,
+          "pitch": 0.31100935232997173,
+          "rotation": 5.497787143782138,
+          "target": "2-img_20230623_123342_082"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6339749563089185,
+          "pitch": 0.07982549565684671,
+          "title": "Estudio de radio escolar",
+          "text": "Zona para grabación de podcast"
+        },
+        {
+          "yaw": -2.25533323543495,
+          "pitch": -0.25772822472681867,
+          "title": "Croma",
+          "text": "Zona para difusión de proyectos audiovisuales"
+        },
+        {
+          "yaw": 3.05986745620854,
+          "pitch": -0.04676406473564043,
+          "title": "Mesa de trabajo para proyectos y maquetas",
+          "text": "Zona de trabajo en grupo para montaje de proyectos"
+        }
+      ]
+    },
+    {
+      "id": "2-img_20230623_123342_082",
+      "name": "IMG_20230623_123342_082",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.095964578506262,
+        "pitch": 0.14924586408711882,
+        "fov": 1.405674899209756
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.011775885190774,
+          "pitch": 0.3234742552434202,
+          "rotation": 5.497787143782138,
+          "target": "1-img_20230623_123348_040"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.801397589143207,
+          "pitch": -0.1838498656356098,
+          "title": "Impresoras 3D",
+          "text": "Zona de impresión 3D"
+        },
+        {
+          "yaw": -2.628599873435647,
+          "pitch": -0.15719061198532458,
+          "title": "Máquina de corte láser",
+          "text": "Zona de corte y grabado láser"
+        }
+      ]
+    }
+  ],
+  "name": "Taller tecnología planta alta",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
